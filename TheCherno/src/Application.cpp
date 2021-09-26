d3e62bd1fc21558669a1cb8ad3ca248be62f92a2
Pedro Mendonca <pedro.mendonca@mvisia.com.br>
2021-09-26 20:17:38 -0300

    Video 02 - GLEW added to project.

--- TheCherno/src/Application.cpp
+++ TheCherno/src/Application.cpp
@@ -1,4 +1,8 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -7,6 +11,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,6 +23,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Glew Init error!" << std::endl;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
