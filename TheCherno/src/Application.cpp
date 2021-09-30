2e09026f06530484ba90d7cb74f90cd30c25231b
Pedro Mendonca <pedrinho.mendonca@gmail.com>
2021-09-30 08:39:48 -0300

    Video 04 - Vertex buffers

--- TheCherno/src/Application.cpp
+++ TheCherno/src/Application.cpp
@@ -1,3 +1,10 @@
+/*
+
+References:
+    The Cherno YouTube channel https://www.youtube.com/channel/UCQ-W1KE9EYfdxhL6S4twUNw
+    The Cherno OpenGL playlist https://www.youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
+    OpenGL documentation https://docs.gl/
+*/
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -30,17 +37,26 @@ int main(void)
     }
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    // Pass data to OpenGL
+    float positions[6] = {
+        -0.5f, -0.5f,
+        +0.0f, +0.5f,
+        +0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);  // qty of buffers and ID
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    // specify size in bytes. Could be sizeof(positions)
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
