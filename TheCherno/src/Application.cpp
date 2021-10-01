345030343af6d90c2c78bc0e84a7dbcea63bbbd9
Pedro Mendonca <pedrinho.mendonca@gmail.com>
2021-09-30 21:52:13 -0300

    Video 07 - Writing a Shader in OpenGL

--- TheCherno/src/Application.cpp
+++ TheCherno/src/Application.cpp
@@ -9,9 +9,71 @@ References:
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <assert.h>
 #include <iostream>
+#include <fstream>
 
-int main(void)
+std::string
+read_text_file(const char* path)
+{
+    std::ifstream file_stream(path);
+    file_stream.seekg(0, std::ios::end);
+    size_t size = file_stream.tellg();
+    std::string buffer(size, ' ');
+    file_stream.seekg(0);
+    file_stream.read(&buffer[0], size);
+    return buffer;
+}
+
+static unsigned int
+compile_shader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result = 0;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length = 0;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        // alloca allocates dynamically on the STACK!
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile shader "
+            << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment")
+            << std::endl << message << std::endl << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int
+create_shader(const std::string& vertex_shader, const std::string& fragment_shader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+    unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
+    assert(vs != 0);
+    assert(fs != 0);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int
+main(void)
 {
     GLFWwindow* window;
 
@@ -37,6 +99,8 @@ int main(void)
     }
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    unsigned int buffer = 0;
+    unsigned int shader = 0;
     {
         float positions[6] = {
             -0.5f, -0.5f,
@@ -44,7 +108,6 @@ int main(void)
             +0.5f, -0.5f
         };
 
-        unsigned int buffer;
         // generate 1 buffer and assign it an id
         glGenBuffers(1, &buffer);
         // select the buffer
@@ -55,6 +118,11 @@ int main(void)
         // Tell OpenGL how to interpret the buffer
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);
+
+        std::string vertex_shader = read_text_file("C:\\dev\\OpenGL\\TheCherno\\TheCherno\\src\\vertex.shader.txt");
+        std::string fragment_shader = read_text_file("C:\\dev\\OpenGL\\TheCherno\\TheCherno\\src\\fragment.shader.txt");
+        shader = create_shader(vertex_shader, fragment_shader);
+        glUseProgram(shader);
     }
 
     /* Loop until the user closes the window */
@@ -74,6 +142,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
